3016ec9d2bd64e5fa5618365bacb8c4410b47a3c
seamussmith <student.141504@worcesterschools.net>
2021-12-15 10:36:50 -0500

    oauth views

--- ratatoskr/ratatoskr/urls.py
+++ ratatoskr/ratatoskr/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view()),
 ]

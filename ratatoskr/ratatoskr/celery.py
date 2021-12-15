a9cc476fba325a7638b77519b9c4923efb2a94ac
Damian J <jadczakdamian@gmail.com>
2021-12-15 10:45:28 -0500

    Starting on setting up Celery

---
+++ ratatoskr/ratatoskr/celery.py
@@ -0,0 +1,20 @@
+import os 
+from celery import Celery
+
+# Set the defauly Django settings module for the 'celery' program
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ratatoskr.settings')
+
+app = Celery('ratatoskr')
+
+# Using a string here means the worker doesn't have to serialize the conifiguration
+# object to child processes. 
+#   - namespace='CELERY' means all celery-related configuration keys
+#     should have a `CELERY_` prefix
+app.config_from_object('django.conf:settomgs', namespace='CELERY')
+
+# Load task modules from all registered Django apps
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')

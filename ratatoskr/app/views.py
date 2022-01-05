09412ada48daaedee51467e8b52e6cc77ff35696
seamussmith <student.141504@worcesterschools.net>
2022-01-05 13:03:49 -0500

    Make sure user is authed

--- ratatoskr/app/views.py
+++ ratatoskr/app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import PermissionDenied
 
 # Create your views here.
 def index(request):
     return render(request, 'app/pages/index.html', {})
 
 def create_schedule(request):
+    if not request.user.is_authenticated:
+        raise PermissionDenied()
     if request.method == "POST":
         pass
     return render(request, 'app/pages/create_schedule.html', {})

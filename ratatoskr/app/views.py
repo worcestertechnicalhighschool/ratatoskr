860b4f12f3ba13113ce2640f6cd800569dcb86d8
seamussmith <student.141504@worcesterschools.net>
2022-01-06 08:16:57 -0500

    Schedule submitting is now a thing

--- ratatoskr/app/views.py
+++ ratatoskr/app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import PermissionDenied
+from django.utils import dateparse, timezone
+from .models import Schedule, TimeSlot, Reservation
+import datetime
 
 # Create your views here.
 def index(request):
@@ -10,5 +13,14 @@ def create_schedule(request):
     if not request.user.is_authenticated:
         raise PermissionDenied()
     if request.method == "POST":
-        request.post
+        lock_date = datetime.datetime.now() + datetime.timedelta(days=99999)
+        if request.POST.get("should_lock_automatically") == "on":
+            lock_date = dateparse.parse_datetime(request.POST.get("auto_lock_after"))
+        Schedule.objects.create(
+            owner = request.user,
+            name = request.POST.get("name"),
+            auto_lock_after = lock_date,
+            is_locked = False
+        )
+
     return render(request, 'app/pages/create_schedule.html', {})

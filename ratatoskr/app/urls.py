3293b8bfe99830d17e881c6e3ab6183720e83b3e
seamussmith <62019857+seamussmith@users.noreply.github.com>
2022-01-19 12:51:55 -0500

    Revert "Schedule viewing lets gooo"

--- ratatoskr/app/urls.py
+++ ratatoskr/app/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create-schedule/', views.create_schedule, name='create-schedule'),
-    path('schedule/<int:schedule_id>', views.schedule, name='schedule'),
 ]
--- ratatoskr/app/views.py
+++ ratatoskr/app/views.py
@@ -1,12 +1,9 @@
-from tokenize import group
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import PermissionDenied
 from django.utils import dateparse, timezone
-from django.utils.timezone import make_aware
 from .models import Schedule, TimeSlot, Reservation
 import datetime
-from itertools import groupby
 
 # Create your views here.
 def index(request):
@@ -24,21 +21,8 @@ def create_schedule(request):
         Schedule.objects.create(
             owner = request.user,
             name = request.POST.get("name"),
-            auto_lock_after = make_aware(lock_date),
+            auto_lock_after = lock_date,
             is_locked = False
         )
 
     return render(request, 'app/pages/create_schedule.html', {})
-
-def schedule(request, schedule_id):
-    schedule = Schedule.objects.get(pk=schedule_id)
-    timeslots = TimeSlot.objects.filter(schedule=schedule)
-
-    return render(request, 'app/pages/schedule.html', {
-        "schedule": schedule,
-        "timeslots": dict(
-            sorted(
-                { k: list(v) for k, v in groupby(timeslots, lambda x: x.time_from.date()) }.items() # Group the timeslots by their time_from date
-            )
-        )
-    })

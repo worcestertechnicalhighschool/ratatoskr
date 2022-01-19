52b172029b2b62eb40dad631d07e8b69188e755d
seamussmith <student.141504@worcesterschools.net>
2022-01-19 11:07:48 -0500

    this is a debug feature

--- ratatoskr/app/views.py
+++ ratatoskr/app/views.py
@@ -33,10 +33,7 @@ def create_schedule(request):
 def schedule(request, schedule_id):
     schedule = Schedule.objects.get(pk=schedule_id)
     timeslots = TimeSlot.objects.filter(schedule=schedule)
-    b = groupby(timeslots, lambda x: x.time_from.date())
-
-    for k, group in b:
-        print(k, list(group))
+    
     return render(request, 'app/pages/schedule.html', {
         "schedule": schedule,
         "timeslots": {k: list(v) for k, v in groupby(timeslots, lambda x: x.time_from.date())}

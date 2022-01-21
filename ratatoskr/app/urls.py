ebc0d7388a5ff6bc8d0f69bef4aa76ca9bd04d67
seamussmith <student.141504@worcesterschools.net>
2022-01-21 10:32:48 -0500

    Make the name make more sense

--- ratatoskr/app/urls.py
+++ ratatoskr/app/urls.py
@@ -12,11 +12,11 @@ class DateConverter:
     def to_url(self, value):
         return value
 
-register_converter(DateConverter, 'yyyy')
+register_converter(DateConverter, 'datetime')
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('create-schedule/', views.create_schedule, name='create-schedule'),
     path('schedule/<int:schedule_id>', views.schedule, name='schedule'),
-    path('schedule/<int:schedule_id>/<yyyy:date>', views.schedule_day, name='schedule-day'),
+    path('schedule/<int:schedule_id>/<datetime:date>', views.schedule_day, name='schedule-day'),
 ]

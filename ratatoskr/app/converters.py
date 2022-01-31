1284f08bfbecc94b68aaeea6c8eb35f3bd3ea86a
seamussmith <student.141504@worcesterschools.net>
2022-01-31 08:37:45 -0500

    Move all converters to a seperate module

---
+++ ratatoskr/app/converters.py
@@ -0,0 +1,13 @@
+from django.urls import register_converter
+from datetime import datetime
+
+class DateConverter:
+    regex = '\d{4}-\d{2}-\d{2}'
+
+    def to_python(self, value):
+        return datetime.strptime(value, '%Y-%m-%d')
+
+    def to_url(self, value):
+        return value
+
+register_converter(DateConverter, 'datetime')

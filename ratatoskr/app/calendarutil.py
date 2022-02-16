37dde740250749c9db37b47d2b8eaaab13141a0c
seamussmith <student.141504@worcesterschools.net>
2022-02-16 08:50:26 -0500

    Start working on calendar event insertion

--- ratatoskr/app/calendarutil.py
+++ ratatoskr/app/calendarutil.py
@@ -3,7 +3,10 @@ from google.oauth2.credentials import Credentials
 from allauth.socialaccount.models import SocialToken, SocialApp
 from django.contrib.auth.models import User
 
-def build_calendar_api(user: User):
+from .models import Reservation, TimeSlot
+
+# Builds the calendar api using the User's api tokens
+def build_calendar_client(user: User):
     token = SocialToken.objects.get(account__user=user, account__provider='google')
     google_app = SocialApp.objects.get(provider="google")
     credentials = Credentials(
@@ -13,3 +16,26 @@ def build_calendar_api(user: User):
         client_id=google_app.client_id, 
         client_secret=google_app.secret) 
     return build('calendar', 'v3', credentials=credentials)
+
+def update_user(timeslot: TimeSlot) -> None:
+    schedule = timeslot.schedule
+    owner = schedule.owner
+    client = build_calendar_client(owner)
+    event = {
+        'summary': 'Ratatoskr: Meeting Reservation',
+        'location': 'Remote',
+        'description': '',
+        'start': {
+            'dateTime': timeslot.time_from.isoformat(),
+            'timeZone': 'America/New_York',
+        },
+        'end': {
+            'dateTime': timeslot.time_to.isoformat(),
+            'timeZone': 'America/New_York',
+        },
+        'attendees': [{'email': t.email} for t in timeslot.reservation_set],
+        'reminders': {
+            'useDefault': True
+        },
+    }
+    

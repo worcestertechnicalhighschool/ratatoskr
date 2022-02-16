e00095a9fd0b3fc1f7d4d3ea5932905727e1710f
seamussmith <student.141504@worcesterschools.net>
2022-02-16 17:30:11 -0500

    Helper functions for creating calendar element IDs

--- ratatoskr/app/calendarutil.py
+++ ratatoskr/app/calendarutil.py
@@ -3,7 +3,29 @@ from google.oauth2.credentials import Credentials
 from allauth.socialaccount.models import SocialToken, SocialApp
 from django.contrib.auth.models import User
 
-from .models import Reservation, TimeSlot
+from .models import Reservation, Schedule, TimeSlot
+
+# Notes:
+# Client object is just a capsule for the Credentials, there is no cost to building multiple client objects
+
+# Template strings for the IDs for each of our calendar elements
+# The ID+DomainName method for generating IDs for our calendar elements should be unique enough so it doesn't clash with any other IDs
+# AutoIncrement fields in SQL never return previous numbers, so we should also be safe in that regard too.
+
+CALENDAR_ID_SUFFIX = "ratatoskr.techhigh.us"
+CALENDAR_SCHEDULE_ID = "{schedule_id}#" + CALENDAR_ID_SUFFIX                        # Ex: "9824#ratatoskr.techhigh.us"
+CALENDAR_TIMESLOT_EVENT_ID = "{timeslot_id}@{schedule_id}#" + CALENDAR_ID_SUFFIX    # Ex: "74343@9824#ratatoskr.techhigh.us"
+
+def make_schedule_id(schedule: Schedule) -> str:
+    return CALENDAR_SCHEDULE_ID % {
+        "schedule_id": schedule.id
+    }
+
+def make_timeslot_event_id(timeslot: TimeSlot) -> str:
+    return CALENDAR_TIMESLOT_EVENT_ID % {
+        "schedule_id": timeslot.id,
+        "timeslot_id": timeslot.schedule.id
+    }
 
 # Builds the calendar api using the User's api tokens
 def build_calendar_client(user: User):

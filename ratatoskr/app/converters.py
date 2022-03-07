ed82deb721fff5120a3de53ab303f9b5c453cae4
seamussmith <student.141504@worcesterschools.net>
2022-03-07 09:40:33 -0500

    Add converter classes

--- ratatoskr/app/converters.py
+++ ratatoskr/app/converters.py
@@ -3,7 +3,7 @@ from django.urls import register_converter
 from django.db import models
 from datetime import datetime
 
-from .models import Schedule
+from .models import Reservation, Schedule, TimeSlot
 
 class DateConverter:
     regex = '\d{4}-\d{2}-\d{2}'
@@ -17,9 +17,9 @@ class DateConverter:
 # Converter class factory for creating URL converter classes
 DIGIT_REGEX = "\d+"
 def create_model_converter(model_class: models.Model, regex: str = DIGIT_REGEX):
-    regex = regex
     class Converter:
-        regex = regex
+        def __init__(self):
+            self.regex = regex
 
         def to_python(self, value):
             return model_class.objects.get(pk=int(value))
@@ -31,3 +31,5 @@ def create_model_converter(model_class: models.Model, regex: str = DIGIT_REGEX):
 
 register_converter(DateConverter, 'datetime')
 register_converter(create_model_converter(Schedule), 'schedule')
+register_converter(create_model_converter(TimeSlot), 'timeslot')
+register_converter(create_model_converter(Reservation), 'reservation')

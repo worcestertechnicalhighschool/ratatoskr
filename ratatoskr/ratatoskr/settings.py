d97eef63aef6bd21f64edb3238fdaa60bf94cf0a
Jack Richard <student.145024@worcesterschools.net>
2022-03-15 10:29:04 -0400

    Moved templates directory (?)

--- ratatoskr/ratatoskr/settings.py
+++ ratatoskr/ratatoskr/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'ratatoskr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

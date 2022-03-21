f05958408be87946b614daaeac47c83915a6eca5
Damian J <jadczakdamian@gmail.com>
2022-03-21 10:56:22 -0400

    400 error page

--- ratatoskr/app/errors.py
+++ ratatoskr/app/errors.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def error404(request, exception):
     return render(request, 'app/error/404.html')
 
+def error400(request, exception):
+    return render(request, 'app/error/400.html')

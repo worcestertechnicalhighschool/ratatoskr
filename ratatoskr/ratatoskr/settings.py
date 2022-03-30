e67533bbd546846b33b3c2c596e94720faaa2a83
Jack Richard <student.145024@worcesterschools.net>
2022-03-30 10:37:47 -0400

    Fixed error page titles.

--- ratatoskr/ratatoskr/settings.py
+++ ratatoskr/ratatoskr/settings.py
@@ -114,7 +114,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -133,7 +132,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -144,7 +142,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/

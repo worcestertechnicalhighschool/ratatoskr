2c6b47f306ea5d4e971c86411000cfd6428b2728
seamussmith <student.141504@worcesterschools.net>
2022-04-04 08:44:28 -0400

    Comment

--- ratatoskr/ratatoskr/threadutil.py
+++ ratatoskr/ratatoskr/threadutil.py
@@ -1,7 +1,9 @@
 
 from threading import Thread
 
-
+# Makes the function being decorated function
+# spawn a thread and execute asynchronously
+# upon being called instead of executing synchronously
 def daemon(func):
     def inner(*args, **kwargs):
         l = lambda: func(*args, **kwargs)

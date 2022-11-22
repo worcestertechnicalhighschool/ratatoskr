14cf6bfbd9813e208328fac5459447c7b6b0d429
Damian Jadczak <jadczakdamian@gmail.com>
2022-11-22 10:44:49 -0500

    Get rid of everything in the file

--- ratatoskr/app/signals.py
+++ ratatoskr/app/signals.py
@@ -1,5 +1,2 @@
-from allauth.account.signals import user_logged_in as allauth_user_logged_in
-from django.contrib.auth import logout 
-from django.contrib import messages
-from ratatoskr.settings import DEBUG
+# Put signal listeners here, if you wanna
     

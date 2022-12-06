bcac354afd6b8bcf88e14893e4a29a9403661b78
Damian Jadczak <jadczakdamian@gmail.com>
2022-12-06 12:16:00 -0500

    added form error page to urls

--- ratatoskr/app/urls.py
+++ ratatoskr/app/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('help/', views.help_page, name='help'),
     path('robots.txt', views.robots, name='robots'),
-    path('privacy/', views.privacy, name="privacy")
+    path('privacy/', views.privacy, name='privacy'),
+    path('form-error/', views.form_error, name='form-error')
 ]
